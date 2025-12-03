fb09580c01ba5fb4d41e20203814c1a8aba2e182
rkendel1 <148293244+rkendel1@users.noreply.github.com>
2025-12-03 15:46:54 -0500

    feat: Add new app 'signapp' from App Wizard

---
+++ apps/signapp/src/pages/_app.jsx
@@ -0,0 +1,26 @@
+
+import { AuthProvider } from '@/auth/AuthProvider';
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
+import '@/styles/globals.css';
+import { useRouter } from 'next/router';
+
+export default function App({ Component, pageProps }) {
+  const router = useRouter();
+  const isWebview = router.query.webview === 'true';
+  const showHeader = false;
+  const showFooter = false;
+
+  return (
+    <AuthProvider>
+      <div className="min-h-screen flex flex-col font-sans">
+        {!isWebview && showHeader && <Header />}
+        <main className="flex-grow">
+          <Component {...pageProps} />
+        </main>
+        {!isWebview && showFooter && <Footer />}
+      </div>
+    </AuthProvider>
+  );
+}
+    
